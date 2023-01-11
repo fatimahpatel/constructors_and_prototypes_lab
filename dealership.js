65a6e8b5de9045deffd81715b34a49c68b0c5e19
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-11 16:21:36 +0000

    Created test to see if car can be added to carStock

--- dealership.js
+++ dealership.js
@@ -1,7 +1,11 @@
-const Dealership = function(name, maxCars, carsInStock){
+const Dealership = function(name, maxCars){
     this.name = name;
     this.maxCars = maxCars;
-    this.carsInStock = carsInStock;
+    carStock = [];
+}
+
+Dealership.prototype.addCarToStock = function(newCar){
+    carStock.push(newCar);
 }
 
 module.exports = Dealership;
--- dealership.test.js
+++ dealership.test.js
@@ -1,18 +1,27 @@
 const Dealership = require('./dealership');
+const Car = require('./Car');
 
 
-let dealership;
+let dealership; //creating new dealership object
+let car;
 beforeEach(() => {
-    dealership = new Dealership("Fatimah's Dealership", 6, null);
+    dealership = new Dealership("Fatimah's Dealership", );
+    car = new Car("Ferrari", 170000, "V8");
+    dealership.addCarToStock(car);
 })
+
 
 describe('testing dealership', () => {
     test('return name of dealership', () => {
         const expected = "Fatimah's Dealership";
         const actual = dealership["name"];
         expect(actual).toBe(expected);
+    })
 
-
+    test('can add car to carStock', () => {
+        const expected = true;
+        const actual = carStock.includes(car);
+        expect(actual).toBe(expected);
     })
 
 
